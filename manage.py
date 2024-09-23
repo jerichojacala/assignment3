56fd816e346652ee8923e2a2e1b1f8225cbfa1bc
jerichojacala <jjacala@bu.edu>
2024-09-22 23:01:28 -0400

    more eduts

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from django.core.wsgi import get_wsgi_application
 """Run administrative tasks."""
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs412.settings')
 """Handler for wsgi for vercel"""
-application = get_wsgi_application()
+app = get_wsgi_application()
 
 def main():
 
