a377ef58d3acb974880e42d57328c4723ae1c80d
jerichojacala <jjacala@bu.edu>
2024-09-29 22:19:21 -0400

    reverted changes to fix assignment 3 deployment

--- cs412/settings.py
+++ cs412/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'quotes', #new addition
+    'quotes', #new addition
     #'formdata', #new app!!!
-    'restaurant',
+    #'restaurant',
 ]
 
 MIDDLEWARE = [
